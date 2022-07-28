613d683a1e4e770bf2e0d42b5bc1bf984f9e5f5a
DESKTOP-JA8MVMV\Desktop <elif.sahin@nanclouds.com>
2022-07-28 22:11:09 +0300

    feat:create daily page

---
+++ src/app/daily-list/daily-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-list',
+  templateUrl: './daily-list.component.html',
+  styleUrls: ['./daily-list.component.css']
+})
+export class DailyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
