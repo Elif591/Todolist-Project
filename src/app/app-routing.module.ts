f5db8d2671b6afb254c363fd28625f4a6601f089
DESKTOP-JA8MVMV\Desktop <elif.sahin@nanclouds.com>
2022-07-29 13:05:23 +0300

    Revert "feat:create project pages"
    This reverts commit 35d47f52c2e5112f975a9abce460dea1f2cb3b7b.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreatetaskComponent } from './createtask/createtask.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DailyMonthlyComponent } from './daily-monthly/daily-monthly.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { CreatetaskComponent } from './createtask/createtask.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DailyMonthlyComponent },
   { path: 'newtask', component: CreatetaskComponent },
   { path: '', redirectTo: '/register', pathMatch: 'full' },
+
 ];
 
 @NgModule({
