35d47f52c2e5112f975a9abce460dea1f2cb3b7b
DESKTOP-JA8MVMV\Desktop <elif.sahin@nanclouds.com>
2022-07-29 13:04:38 +0300

    feat:create project pages

---
+++ src/app/monthly-list/monthly-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monthly-list',
+  templateUrl: './monthly-list.component.html',
+  styleUrls: ['./monthly-list.component.css']
+})
+export class MonthlyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
